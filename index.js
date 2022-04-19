5df39a0a46ccab676693b193e1029be9cc4d4be4
nkunic <natasa.kunic@gmail.com>
2022-04-19 23:16:01 +0200

    1 file modified

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ console.log(img);
 var counter = 1;
 
 var loop = setInterval(function(){
+
   counter++; // 2
+
   // if(counter == 4) {
   //   counter == 1;
   // }
+
   (counter == 4) ? (counter = 1) : (counter = counter);
+  
   img.setAttribute("src", "https://raw.githubusercontent.com/nkunic/js-example-slider/master/images/bg-img" + counter + ".jpg");
 },2000)

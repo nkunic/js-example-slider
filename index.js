7196c067dfedd20cfdec7ae708b972fd67ba88c7
nkunic <natasa.kunic@gmail.com>
2022-04-19 22:06:10 +0200

    3 files modified

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 import './style.css';
 
 // Write Javascript code!
- var img = document.querySelector('img');
- console.log(img);
+var img = document.querySelector('img');
+console.log(img);
 
- var counter = 1;
+var counter = 1;
 
- var loop = setInterval(function(){
+var loop = setInterval(function(){
   counter++; // 2
+  // if(counter == 3) {
+  //   counter == 1;
+  // }
+  (counter == 3) ? (counter == 1) : (counter == counter);
   img.setAttribute("src", "images/bg-img" + counter + ".jpg");
- },2000)
+},2000)
